beab620ca5bd46223bf4d325c668d6d20e1215b2
George Hamilton <gmh04@netscape.net>
2017-11-29 15:40:02 +0000

    add post test

--- risk/tests.py
+++ risk/tests.py
@@ -9,10 +9,9 @@ class ApiTest(TestCase):
     fixtures = ['risk/fixtures/data.json',]
 
     def test_api(self):
-        print (settings.BASE_DIR)
+        # with no risk defined
         response = self.client.get('/api/')
         self.assertEqual(response.status_code, 200)
-
         self.assertJSONEqual(
             str(response.content, encoding='utf8'),
             {'msg': 'Risk needs to be defined'}
@@ -22,9 +21,17 @@ class ApiTest(TestCase):
         response = self.client.get('/api/automobiles')
         content = json.loads(str(response.content, encoding='utf8'))['fields']
         self.assertTrue(len(content), 5)
-        self.assertEquials(content[3]['value'], 'ford,seat,kia')
+        self.assertEquals(content[3]['value'], 'ford,seat,kia')
 
         # entry with no non generic fields with have the 3 common enties
         response = self.client.get('/api/other')
         content = json.loads(str(response.content, encoding='utf8'))['fields']
         self.assertTrue(len(content), 3)
+
+        # post returns 501
+        response = self.client.post('/api/other')
+        self.assertEqual(response.status_code, 501)
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            {'msg': 'To be implemented'}
+        )
