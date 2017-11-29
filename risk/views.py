5bacec08fffaeb0870693b4a4928f04cf3480135
George Hamilton <gmh04@netscape.net>
2017-11-29 12:29:13 +0000

    do some tidying up

--- risk/views.py
+++ risk/views.py
@@ -1,18 +1,15 @@
+from django.db.models import Q
 from django.http import JsonResponse
+from django.shortcuts import render
 
 from risk.models import RiskValue
-
-#import json
-from django.db.models import Q
-
-
-from django.shortcuts import render
 
 def api(request, risk=None):
     if request.method == 'GET':
         if risk:
-            obj = RiskValue.objects.filter(Q(entity__name='common') | Q(entity__name=risk))
-            context = {'latest_question_list': obj}
+            # retieve all common and 'risk' defined RiskValue entries
+            vals = RiskValue.objects.filter(Q(entity__name='common') | Q(entity__name=risk))
+            context = {'entries': vals}
             return render(request, 'api.json', context, content_type='application/json')
         else:
             return JsonResponse({'msg': 'Risk needs to be defined'})
